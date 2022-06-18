5e2aef1aba1742d8e592446a632c821b9b5a3794
Davner <davner23@gmail.com>
2022-06-18 04:05:31 -0400

    Início do E-commerce Clothing

---
+++ public/js/nav.js
@@ -0,0 +1,10 @@
+const CREATE_NAVBAR = () => {
+    let nav = document.querySelector('.navbar');
+    nav.innerHTML = `
+    
+    <img src="/public/img/dark-logo.png" alt="">
+    
+    `;
+}
+
+CREATE_NAVBAR();
